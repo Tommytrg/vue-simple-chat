11f1e80813b12a4154ca48aa65057307e1cf08fc
tommytrg <tomastrg22@gmail.com>
2019-12-10 22:54:59 +0100

    wip

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="chat">
-    <div class="date">Hoy</div>
-    <div class="message-list">
-      <div v-for="msg in msgList" :key="msg.id" class="message" :class="[msg.participant]">
-        <div v-if="msg.participant === 'internal'" class="time"> 12.20 </div>
-        <div class="text" :class="[msg.participant]">
-          <p>{{ msg.text }}</p>
+    <div
+      class="message-list"
+      ref="messageList"
+    >
+      <div
+        v-for="(msg, index) in msgList"
+        :key="msg.id"
+        class="row"
+        :ref="`message-${index}`"
+        :data-index="index"
+      >
+        <div v-if="isDifferentDate(index)" class="date">{{ formatDate(msg.date) }}</div>
+        <div class="message" :class="[msg.participant]">
+          <div v-if="msg.participant === 'internal'" class="time">12.20</div>
+          <div class="text" :class="[msg.participant]">
+            <p>{{ msg.text }}</p>
+          </div>
+          <div v-if="msg.participant === 'external'" class="time">12.20</div>
         </div>
-        <div v-if="msg.participant === 'external'" class="time"> 12.20 </div>
       </div>
     </div>
     <form class="message-bar" v-on:submit.prevent="onSubmit">
@@ -29,7 +40,19 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from "vue"
+import dayjs from 'dayjs'
+import calendar from 'dayjs/plugin/calendar'
+import 'dayjs/locale/es'
+
+dayjs.extend(calendar)
+type Message = {
+  id: number,
+  text: string,
+  participant: 'internal' | 'external'
+  time: string,
+  date: string
+}
 
 export default Vue.extend({
   name: "Chat",
@@ -37,13 +60,44 @@ export default Vue.extend({
     msgList: {
       type: Array,
       default: () => [
-        { id: 0, text: "Hola", participant: "internal" },
-        { id: 10, text: "Hola", participant: "internal" },
-        { id: 110, text: "Hola", participant: "internal" },
-        { id: 11110, text: "Hola", participant: "internal" },
-        { id: 230, text: "Hola", participant: "internal" },
-        { id: 40, text: "Hola", participant: "internal" },
-        { id: 51, text: `Hola adsfadsf
+        {
+          id: 110,
+          text: "Hola",
+          participant: "internal",
+          time: "10.20",
+          date: 1575148557000
+        },
+        {
+          id: 11111,
+          text: "Hola",
+          participant: "internal",
+          time: "10.20",
+          date: 1575321357000 
+        },
+        {
+          id: 11116,
+          text: "Hola",
+          participant: "internal",
+          time: "10.20",
+          date: 1575580557000
+        },
+        {
+          id: 230,
+          text: "Hola",
+          participant: "internal",
+          time: "10.20",
+          date: 1575839757000
+        },
+        {
+          id: 40,
+          text: "Hola",
+          participant: "internal",
+          time: "10.20",
+          date: 1575926157000
+        },
+        {
+          id: 51,
+          text: `Hola adsfadsf
         
        asdfadsfadfAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
@@ -54,19 +108,53 @@ sfdasdf
        adsfadsfadfs
        
        asfdadsfafd
-        `
-        , participant: "external" },
+        `,
+          participant: "external",
+          time: "10.20",
+          date: 1576012557000
+        }
       ]
     }
   },
   data() {
+    const defaultMessage = 'Write a message'
     return {
       internalMessage: "",
-      message: "Escribe un mensaje...",
-      placeholder: "Escribe un mensaje..."
-    };
+      message: defaultMessage,
+      placeholder: defaultMessage
+    }
   },
   methods: {
+    formatDate(date: Date) {
+      dayjs().calendar(dayjs(date))
+      return dayjs(date).locale('en').calendar(undefined, {
+        sameDay: '[Today]', 
+        lastDay: '[Yesterday]', // The day before ( Yesterday at 2:30 AM )
+        lastWeek: 'dddd', // Last week ( Last Monday at 2:30 AM )
+        sameElse: 'DD/MM/YYYY' // Everything else ( 7/10/2011 )
+      })
+    },
+    isDifferentDate(index: number) {
+      return index === 0 || (this.msgList as Array<Message>)[index].date !== (this.msgList as Array<Message>)[index - 1].date
+    },
+    checkInView(container: any, element: any, partial: boolean = true) {
+      //Get container properties
+      let cHeight = container.clientHeight;
+      let cTop = container.scrollTop;
+      let cBottom = cTop + cHeight;
+
+      //Get element properties
+      let eTop = element.offsetTop;
+      let eBottom = eTop + element.clientHeight;
+
+      //Check if in view
+      let isTotal = eTop >= 0 && eBottom <= cHeight;
+      let isPartial =
+        partial && ((eTop < 0 && eBottom > 0) || (eTop > 0 && eTop <= cHeight));
+
+      //Return outcome
+      return isTotal || isPartial;
+    },
     addCarriageReturn(event: Event) {
       event.preventDefault();
       console.log("----");
@@ -90,10 +178,9 @@ sfdasdf
 
 <style lang="scss">
 .chat {
-  border: 1px solid black;
   display: grid;
   grid-template-columns: auto;
-  grid-template-rows: auto 250px auto;
+  grid-template-rows: 275px auto;
   height: 335px;
   justify-content: center;
   padding: 8px 32px;
@@ -103,19 +190,19 @@ sfdasdf
     font-size: 14px;
     font-weight: 900;
     text-align: center;
+    padding: 8px;
   }
 
   .message-list {
     padding: 0 8px;
-    // height: max-content;
     display: flex;
     flex-direction: column;
-    overflow: scroll;
     overflow-x: hidden;
-    overflow-y: auto;
+    overflow-y: scroll;
     margin: 8px 0;
 
     .message {
+      padding: 4px 0;
       display: flex;
       align-content: flex-end;
 
@@ -126,8 +213,8 @@ sfdasdf
         border: 1px solid black;
         display: flex;
         border-radius: 28px;
-        box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.25);
-        
+        // box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.25);
+
         p {
           padding: 0;
           margin: 0;
@@ -141,7 +228,6 @@ sfdasdf
       &.external {
         justify-content: flex-start;
       }
-
 
       .text {
         max-width: 300px;
@@ -161,6 +247,7 @@ sfdasdf
   }
 
   .message-bar {
+    border-radius: 10px;
     border: 1px solid black;
     display: grid;
     grid-template-columns: 1fr auto;
@@ -178,7 +265,6 @@ sfdasdf
       grid-template-rows: 1fr;
       max-height: 80px;
       outline: none;
-
     }
 
     .hidden {
